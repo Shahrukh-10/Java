7a75278d076b9b7da6973342adcc853f1f98dbf8
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2020-12-07 21:18:22 -0800

    Update getDay.java

--- getDay.java
+++ getDay.java
@@ -10,6 +10,7 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 import java.time.LocalDate;
+
 class Result {
 
     
